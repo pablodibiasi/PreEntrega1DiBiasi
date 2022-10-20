8626a7c755f4722c22f57d951eb3e5220e1f4652
pablo di biasi <licdibiasi@gmail.com>
2022-10-20 12:27:28 -0300

    metiendo material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css'
-import Navbar from './components/NavBar'
+import ResponsiveAppBar from './components/NavBar'
 
 function App() {
-  function funcionLogin() {
-    alert('algo importante sobre el login')
-  }
   return (
     <div>
-      <Navbar conBoton={true} color={'#ddd'} funcionLogin={funcionLogin} />
+      <ResponsiveAppBar />
     </div>
   )
 }
